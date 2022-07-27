fd09c548ce7e9f544085587e8048d4f019363205
Sue <100839777+sue-creator@users.noreply.github.com>
2022-07-27 19:13:17 +0900

    Create 1st_prob3.py

---
+++ 1st_prob3.py
@@ -0,0 +1,47 @@
+from collections import deque
+
+result = 0
+
+
+def solution(order):
+    sub = [0]                   ##### stack 구조로
+    global result
+    q = deque(order)
+    max_q = max(q)
+
+    for i in list(range(1, max_q+1)):
+        if len(q) == 0:
+            break
+        if i == q[0]:
+
+            previous_item = q.popleft()
+            result += 1
+
+            if sub_searching(q, sub, max_q) == 0:
+                continue
+                ######## break 했을때는 안됐음
+                ######## 내가 만든 테스트 케이스는 통과됐음. 먼 차이?
+            else:
+                sub_searching(q, sub, max_q)
+        else:
+            sub.append(i)
+    return result
+
+
+def sub_searching(queue, sub_li, N):
+    global result
+    if result >= N:
+        pass
+    if len(sub_li) == 1 or len(queue) == 0:
+        return N
+    if queue[0] == sub_li[-1]:
+        sub_li.pop()
+        queue.popleft()
+        result += 1
+        sub_searching(queue, sub_li, N)
+    else:
+        return 0
+    return N
+
+
+print(solution([1, 2, 3, 4, 5]))
